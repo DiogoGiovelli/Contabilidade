1a28c25ae60d4d8a0d9e2335383cfea1152bc697
DiogoGiovelli <diogogiovelli@gmail.com>
2021-11-23 20:52:36 -0400

    inicial

--- servidorHttp.cs
+++ servidorHttp.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 class ServidorHttp
 {
@@ -16,16 +19,50 @@ class ServidorHttp
      //
         public ServidorHttp (int porta = 8080)
         {
-        this.Porta = porta;
-        try
+            this.Porta = porta;
+            try
+            {
+                  //criando um novo opjeto TcpListener que vai escutar no ip("127.0.0.1") e na porta(this.Porta) passadas
+                  this.Controlador = new TcpListener( IPAddress.Parse("127.0.0.1"), this.Porta);
+                  //inicia a escuta
+                  this.Controlador.Start();
+                  Console.WriteLine($" O Servidor está rodando, acesse http://{this.Controlador}:{this.Porta}/");
+
+            }
+            catch (Exception e)
+            {
+                  Console.WriteLine($"Erro ao iniciar servidor na porta {this.Porta}:\n{e.Message}");
+            }
+        }
+         
+
+         //metodo que vai aguardar novas requisições que vai gerar um objeto do tipo Socket 
+         //Socket é um objeto comum pra comum para comunicação em rede
+         //permite verificar e responder requisições 
+        private async Task AguardarRequests()
         {
-        this.Controlador = new TcpListener( IPAddress.Parse("127.0.0.1"), this.Porta);
+           while (true)
+           {
+              Socket conexao = await this.Controlador.AcceptSocketAsync();
+              this.QtdRequests++;
+              Task task =Task.Run(() => ProcessarRequests(conexao, this.QtdRequests));
+           }
+
         }
-        catch
+
+        private void ProcessarRequests(Socket conexao, int numeroRequest)
         {
-            
+           if (conexao.Connect)
+           {
+           byte[] bytesRequisicao = new byte[1024];
+           conexao.Receive(bytesRequisicao,bytesRequisicao.Length,0);
+           string 
+           }
+
         }
-        }
+
+
+
 
 }
 
